8dde88ad13264d03f4214f4ab33b50dadfef574c
Joshua Tully <seekndestroyer@hotmail.com>
2015-11-14 17:58:35 -0600

    Neighbor counting works and displays both images.

--- CellularAutomata/ca1.py
+++ CellularAutomata/ca1.py
@@ -1,11 +1,22 @@
-import numpy as np
+﻿import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
+def count_neighbors(nda):
+    n = np.zeros(nda.shape,  dtype= int)
+    n += np.roll(nda, nda.shape[0])
+    n += np.roll(nda, -nda.shape[0])
+    n += np.roll(nda, 1, 1)
+    n += np.roll(nda, -1, 1)
+    return n
 width = 8
 height = 8
 ca = np.random.randint(0, 2, (width, height))
 print(ca)
-fig, ax = plt.subplots()
-ax.imshow(ca, interpolation='nearest', cmap="Greys")
+fig, (ax1, ax2) = plt.subplots(1, 2)
+
+ax1.matshow(ca, interpolation='nearest', cmap="viridis")
+print(count_neighbors(ca))
+im = ax2.matshow(count_neighbors(ca), interpolation='nearest', cmap="viridis")
+fig.colorbar(im, ax=ax2)
 plt.show()
